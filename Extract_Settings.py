dfc3a266d9aadfc6f12d2d3296f3aff28567e872
KLRussell <47677315+KLRussell@users.noreply.github.com>
2019-04-16 14:21:42 -0400

    bug fixes

--- Extract_Settings.py
+++ Extract_Settings.py
@@ -21,7 +21,7 @@ class MainGUI:
         self.var = StringVar()
 
     def buildgui(self):
-        self.root.geometry('325x270+500+300')
+        self.root.geometry('300x270+500+300')
         self.root.title('Extract Settings')
 
         top_frame = Frame(self.root)
@@ -39,7 +39,9 @@ class MainGUI:
         scrollbar2 = Scrollbar(self.root, orient="horizontal")
         self.listbox = Listbox(self.root, selectmode=SINGLE, width=40, yscrollcommand=scrollbar.set,
                                xscrollcommand=scrollbar2.set)
+
         self.populatebox()
+
         scrollbar.config(command=self.listbox.yview)
         scrollbar2.config(command=self.listbox.xview)
         scrollbar.pack(in_=middle_frame, side="right", fill="y")
@@ -66,16 +68,134 @@ class MainGUI:
             self.listbox.insert("end", i[0])
 
     def change_settings(self):
-        if self.listbox.curselection():
-            print('hi')
+        if self.listbox.size() > 0 and self.listbox.curselection():
+            if self.obj:
+                self.obj.close()
+
+            self.obj = SettingsGUI(self.root, self.listbox.get(self.listbox.curselection()))
+            self.obj.buildgui()
+        elif self.listbox.size() > 0:
+            messagebox.showerror('Settings Empty Error!', 'No settings have been saved. Please run Extract_Upload.py')
         else:
-            messagebox.showerror('Selection Error!', 'No shelf date was selected. Please select a valid shelf item')
+            messagebox.showerror('Selection Error!', 'No table settings was selected. Please select from list')
 
     def cancel(self):
         if self.obj:
             self.obj.close()
 
         self.root.destroy()
+
+
+class SettingsGUI:
+    entry1 = None
+    listbox = None
+    selection = 0
+
+    def __init__(self, root, table):
+        assert root
+        assert table
+        self.dialog = Toplevel(root)
+        self.rvar = IntVar()
+        self.evar = StringVar()
+        self.table = table
+        configs = Global_Objs['Local_Settings'].grab_item('Accdb_Configs')
+
+        for config in configs:
+            if config[0] == table:
+                self.config = config
+                break
+
+        assert self.config
+
+    def buildgui(self):
+        self.dialog.geometry('250x200+500+300')
+        self.dialog.title('Update TBL Settings')
+
+        top_frame = Frame(self.dialog)
+        middle_frame = Frame(self.dialog)
+        middle_frame2 = Frame(self.dialog)
+        bottom_frame = Frame(self.dialog)
+        top_frame.pack(side=TOP)
+        middle_frame.pack()
+        middle_frame2.pack()
+        bottom_frame.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+        header = Message(self.dialog, text='Please input custom settings for access table {0}:'.format(self.table),
+                         width=240)
+        header.pack(in_=top_frame)
+
+        label1 = Label(self.dialog, text='SQL Server TBL: ', pady=7)
+        self.entry1 = Entry(self.dialog)
+        label1.pack(in_=middle_frame, side=LEFT)
+        self.entry1.pack(in_=middle_frame, side=RIGHT)
+
+        scrollbar = Scrollbar(self.dialog, orient="vertical")
+        scrollbar2 = Scrollbar(self.dialog, orient="horizontal")
+        self.listbox = Listbox(self.dialog, selectmode=SINGLE, width=40, yscrollcommand=scrollbar.set,
+                               xscrollcommand=scrollbar2.set)
+
+        self.populatefields()
+
+        scrollbar.config(command=self.listbox.yview)
+        scrollbar2.config(command=self.listbox.xview)
+        scrollbar.pack(in_=middle_frame2, side="right", fill="y")
+        scrollbar2.pack(in_=middle_frame2, side="bottom", fill="x")
+        self.listbox.pack(in_=middle_frame2, pady=5)
+        self.listbox.bind("<Down>", self.on_list_down)
+        self.listbox.bind("<Up>", self.on_list_up)
+        self.listbox.bind('<<ListboxSelect>>', self.on_select)
+        self.listbox.select_set(self.selection)
+
+        btn = Button(self.dialog, text="Save Settings", width=10, command=self.save_settings)
+        btn2 = Button(self.dialog, text="Cancel", width=10, command=self.close)
+        btn.pack(in_=bottom_frame, side=LEFT, padx=10)
+        btn2.pack(in_=bottom_frame, side=RIGHT, padx=10)
+        self.dialog.mainloop()
+
+    def populatefields(self):
+        self.entry1.insert(0, self.config[2])
+        for from_col, to_col in zip(self.config[1], self.config[3]):
+            self.listbox.insert("end", '{0} => {1}'.format(from_col, to_col))
+
+    def on_select(self, event):
+        if -1 < self.selection < self.listbox.size() - 1:
+            self.selection = self.listbox.curselection()[0]
+
+    def on_list_down(self, event):
+        if self.selection < self.listbox.size()-1:
+            self.listbox.select_clear(self.selection)
+            self.selection += 1
+            self.listbox.select_set(self.selection)
+
+    def on_list_up(self, event):
+        if self.selection > 0:
+            self.listbox.select_clear(self.selection)
+            self.selection -= 1
+            self.listbox.select_set(self.selection)
+
+    def save_settings(self):
+        if self.entry1.get() == "General_Settings_Path":
+            messagebox.showerror('Table Error', 'Unable to have General_Settings_Path as a table name')
+        elif self.entry1.get() == 'Server':
+            messagebox.showerror('Table Error', 'Unable to have Server as a table name')
+        elif self.entry1.get() == 'Database':
+            messagebox.showerror('Table Error', 'Unable to have Database as a table name')
+        else:
+            if self.rvar.get() == 1:
+                myitems = [True, self.entry2.get()]
+            else:
+                myitems = [False, self.entry2.get()]
+
+            if self.entry1.get() in self.local_settings:
+                Global_Objs['Local_Settings'].del_item(self.entry1.get())
+
+            if self.rvar.get() != 1 or self.entry2.get() != '14':
+                Global_Objs['Local_Settings'].add_item(self.entry1.get(), myitems)
+
+            self.dialog.destroy()
+
+    def close(self):
+        self.dialog.destroy()
 
 
 if __name__ == '__main__':
